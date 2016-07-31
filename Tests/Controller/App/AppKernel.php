18e4c525c0045dc33d9149a017a42db95d4e94f6
iCodr8 <icodr8@gmail.com>
2016-07-31 22:30:17 +0200

    removed security bundle

--- Tests/Controller/App/AppKernel.php
+++ Tests/Controller/App/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Craffft\SingleSessionStorageBundle\SingleSessionStorageBundle(),
         );
 

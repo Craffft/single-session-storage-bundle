2bcd524a6e8cb341d830ef7d2a313ccd1ab47f38
Daniel Kiesel <icodr8@gmail.com>
2018-01-29 20:10:22 +0100

    optimized contao manager support

--- ContaoManager/Plugin.php
+++ ContaoManager/Plugin.php
@@ -1,7 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Craffft\SingleSessionStorageBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\SingleSessionStorageBundle\CraffftSingleSessionStorageBundle;
 
 class Plugin
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftSingleSessionStorageBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['translation-fields']),
+        ];
+    }
 }
